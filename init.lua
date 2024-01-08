9ca6415e671e721f8359077d3b616c512c73c0a7
Andrei Mahalean <andrei@maha.nz>
2024-01-09 00:02:58 +1300

    easier netrw

--- init.lua
+++ init.lua
@@ -427,6 +427,7 @@ vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc
 vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>-', ':e %:h<CR>', { noremap = true, desc = "Open explorer in current directory" })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
