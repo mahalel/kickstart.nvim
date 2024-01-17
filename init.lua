b89df9e1e3a55717c691bbe83990abac99438c44
Andrei Mahalean <andrei@maha.nz>
2024-01-18 09:35:20 +1300

    fix hcl

--- init.lua
+++ init.lua
@@ -356,6 +356,18 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
+-- [[ Fix Terraform and HCL comment strings ]]
+-- See https://neovim.discourse.group/t/commentstring-for-terraform-files-not-set/4066/2
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("FixTerraformCommentString", { clear = true }),
+  callback = function(ev)
+    vim.bo[ev.buf].commentstring = "# %s"
+  end,
+  pattern = { "terraform", "hcl" },
+})
+
+
+
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
